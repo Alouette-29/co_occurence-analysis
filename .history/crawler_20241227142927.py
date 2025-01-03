02265341c4e74f688853b4890af1bd4e163b7fa9
Alouette <xby_kusurihimeko@163.com>
2025-01-03 10:03:58 +0800

    done

--- .history/crawler_20241227142927.py
+++
@@ -1,7 +0,0 @@
-import os
-
-
-if __name__ == "__main__":
-    book_url = "https://luoxiadushu.com/shuyi/"
-    if not os.path.exists(book_url):
-        os.makedirs(book_url)
--- .history/person_analyse_20241227172226.py
+++
@@ -1,8 +0,0 @@
-import os
-
-script_dir = os.path.dirname(__file__)
-os.chdir(script_dir)
-
-
-if __name__ == "__main__":
-    file_path = "book"
